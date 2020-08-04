6ab560b7e265b707f020e2f0efa9a4d843815655
michael <michaelbarry564@gmail.com>
2020-08-04 21:03:58 +0100

    Im not really sure what I've changed at this point, added something to the gui

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,13 @@
 package sample;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class Controller {
+
+  @FXML
+  private TextField usernameField;
+
+  @FXML
+  private TextField passwordField;
+
 }
