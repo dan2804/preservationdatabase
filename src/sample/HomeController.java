f8fc3bcd311afdde865fcfe929d599656f15cc99
Michael <michaelbarry564@gmail.com>
2020-08-05 22:36:25 +0100

    proof of concept hello worldy

--- src/sample/HomeController.java
+++ src/sample/HomeController.java
@@ -2,10 +2,16 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 public class HomeController {
 
   @FXML
   private TextField searchTxtField;
+
+  @FXML
+  void search(KeyEvent event) {
+    System.out.println("Hello Dan!");
+  }
 
 }
