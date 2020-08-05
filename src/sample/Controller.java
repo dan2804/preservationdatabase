92936bbced190e405056b398007db15ed355167e
Michael <michaelbarry564@gmail.com>
2020-08-05 04:20:30 +0100

    added login button

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,11 +1,15 @@
 package sample;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class Controller {
 
   @FXML
   private TextField usernameField;
+
+  @FXML
+  private Button loginBtn;
 
   @FXML
   private TextField passwordField;
